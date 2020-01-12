be38424ffadac48d17298b437f8270cc64c2f82a
Gyuri Horak <dyuri@horak.hu>
2020-01-12 12:13:15 +0100

    cie94 distance

--- repacolors/convert.py
+++ repacolors/convert.py
@@ -32,6 +32,10 @@ D55 = (95.682, 100.000, 92.149)
 D65 = (95.047, 100.000, 108.883)
 D75 = (94.972, 100.000, 122.638)
 
+# weighting factors
+Wgraphic = (1, 0.045, 0.015)
+Wtextile = (2, 0.048, 0.014)
+
 CTuple = Tuple[float, ...]
 
 
@@ -79,14 +83,32 @@ class CMYKTuple(NamedTuple):
 
 
 def distance(c1: CTuple, c2: CTuple) -> float:
-    """Color distance
-    Can be used with RGB or CIE-Lab values (CIE76 later case)
+    """Color distance (eucledian)
+    Can be used with RGB or CIE-Lab values (CIE76)
     https://en.wikipedia.org/wiki/Color_difference#CIELAB_%CE%94E*
 
     For Lab colors: diff ~ 2.3 - just noticeable difference
     """
 
     return ((c1[0] - c2[0]) ** 2 + (c1[1] - c2[1]) ** 2 + (c1[2] - c2[2]) ** 2) ** 0.5
+
+
+def distance_cie94(lab1: LabTuple, lab2: LabTuple, w: Tuple[float, float, float] = Wgraphic) -> float:
+    """Color distance
+    CIE94
+    https://en.wikipedia.org/wiki/Color_difference#CIE94
+    """
+
+    dL = lab1.l - lab2.l
+    c1 = (lab1.a ** 2 + lab1.b ** 2) ** .5
+    c2 = (lab2.a ** 2 + lab2.b ** 2) ** .5
+    dC = c1 - c2
+    dH2 = (lab1.a - lab2.a) ** 2 + (lab1.b - lab2.b) ** 2 - dC ** 2
+    dH = 0 if dH2 <= 0 else dH2 ** .5
+    sC = 1 + w[1] * c1
+    sH = 1 + w[2] * c1
+
+    return ((dL / w[0]) ** 2 + (dC / sC) ** 2 + (dH / sH) ** 2) ** .5
 
 
 def ansi2rgb(ansicolor: int) -> RGBTuple:
--- tests/convert/test_distance.py
+++ tests/convert/test_distance.py
@@ -33,3 +33,29 @@ def test_distance_triangle():
         d13 = distance(c1, c3)
 
         assert d12 + d23 >= d13
+
+
+def test_distance_cie94_same():
+    for _ in range(100):
+        c = LabTuple(100 * random.random(), random.random() - .5, random.random() - .5)
+        assert distance_cie94(c, c) == 0
+
+
+def test_distance_cie94_not_same():
+    for _ in range(100):
+        c1 = LabTuple(100 * random.random(), random.random() - .5, random.random() - .5)
+        c2 = LabTuple(100 * random.random(), random.random() - .5, random.random() - .5)
+        if c1 != c2:
+            assert distance_cie94(c1, c2) > 0
+
+
+def test_distance_cie94_triangle():
+    for _ in range(100):
+        c1 = LabTuple(100 * random.random(), random.random() - .5, random.random() - .5)
+        c2 = LabTuple(100 * random.random(), random.random() - .5, random.random() - .5)
+        c3 = LabTuple(100 * random.random(), random.random() - .5, random.random() - .5)
+        d12 = distance_cie94(c1, c2)
+        d23 = distance_cie94(c2, c3)
+        d13 = distance_cie94(c1, c3)
+
+        assert d12 + d23 >= d13
